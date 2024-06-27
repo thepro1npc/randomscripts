c20c58bd70dfa9b6dbb96317a318e2f1060b1fc1
npc <102553808+thepro1npc@users.noreply.github.com>
2024-06-26 23:22:05 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -15,13 +15,13 @@ local Window = Rayfield:CreateWindow({
     },
     KeySystem = false, -- Set this to true to use our key system
     KeySettings = {
-       Title = "TEST TITLE",
+       Title = "MADE BY npc",
        Subtitle = "Key System",
-       Note = "No method of obtaining the key is provided",
+       Note = "Get the key here! https://link-target.net/1195296/script-key",
        FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
        SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-       Key = {"test"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+       GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+       Key = {"https://raw.githubusercontent.com/thepro1npc/randomscripts/main/testing"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
     }
 })
 
