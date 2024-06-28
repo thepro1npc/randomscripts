ea98487d3b4dee5287ae41194f8ac511e0470dcb
npc <102553808+thepro1npc@users.noreply.github.com>
2024-06-27 21:30:01 -0400

    Update z.lua

--- z.lua
+++ z.lua
@@ -11,7 +11,7 @@ local Window = Rayfield:CreateWindow({
     Discord = {
        Enabled = false,
        Invite = "robloxtrollers", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+       RememberJoins = false -- Set this to false to make them join the discord every time they load it up
     },
     KeySystem = false, -- Set this to true to use our key system
     KeySettings = {
@@ -27,22 +27,20 @@ local Window = Rayfield:CreateWindow({
 
 local groupId = 10261023
 local hrRankNames = {
-    "Guest", "Trainee", "Junior Operator", "Senior Operator", "Junior Director", "Senior Director", "Head Director", "Corporate Intern", 
+    "Guest", "Trainee", "Junior Director", "Senior Director", "Head Director", "Corporate Intern", 
     "Junior Corporate", "Senior Corporate", "Head Corporate", "Chief Human Resources Officer",
     "Chief Public Relations Officer", "Chief Operating Officer", "Chief Administrative Officer",
     "Developer", "Vice Chairman", "Chairman"
 }
 
-local mrRankNames = {
-    "Shift Leader", "Supervisor", "Assistant Manager", "General Manager"
-}
+local toggleEnabled = false  -- Initial state of toggle
 
 local function sendNotification(title, message)
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = title,
         Text = message,
         Duration = 10,
-        Button1 = "Ok"
+        Button1 = "OK"
     })
 end
 
@@ -71,7 +69,9 @@ local function checkAndNotifyHR(player)
     if success then
         for _, rankName in ipairs(hrRankNames) do
             if rankName == rank then
-                sendNotification("HR Join", player.Name .. " (" .. rank .. ") has joined the game.")
+                if toggleEnabled then
+                    sendNotification("HR Join", player.Name .. " (" .. rank .. ") has joined the game.")
+                end
                 break
             end
         end
@@ -79,21 +79,25 @@ local function checkAndNotifyHR(player)
 end
 
 -- Notify that the script is working
-sendNotification("NOTIFICATION", "Scanner is WORKING")
+sendNotification("NOTIFICATION", "Thanks for using my script :)")
 
 -- Listen for new players joining
 game.Players.PlayerAdded:Connect(checkAndNotifyHR)
 
-local function flingAll()
-    for _, v in pairs(workspace.SpawnedCars:GetChildren()) do
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.PrimaryPart.CFrame
-        task.wait(0.2)
+-- Example toggle implementation (replace with your actual toggle logic)
+local Toggle = Tab:CreateToggle({
+    Name = "Toggle HR Notifier",
+    CurrentValue = false,
+    Flag = "Toggle HR",
+    Callback = function(Value)
+        toggleEnabled = Value
+        if toggleEnabled then
+            sendNotification("HR Notifier Toggle", "You will be notified whenever an HR joins the game.")
+        else
+            sendNotification("HR Notifier Toggle", "You will NOT be notified whenever an HR joins the game.")
+        end
     end
-end
-
- local Tab = Window:CreateTab("Main Page", 4483362458) -- Title, Image
-
- local Section = Tab:CreateSection("Main Page")
+})
 
  local Button = Tab:CreateButton({
     Name = "Fling Users in Cars",
@@ -122,22 +126,6 @@ end
 		game.Players.LocalPlayer.GroupInfo.Rank.Value = 255
     end,
  })
-
- local Toggle = Tab:CreateToggle({
-    Name = "HR Notifier",
-    CurrentValue = false,
-    Flag = "HRNotifierToggle",
-    Callback = function(Value)
-        hrNotifierEnabled = Value
-        if hrNotifierEnabled then
-            sendNotification("NOTIFICATION", "HR Notifier is ENABLED")
-            -- Listen for new players joining
-            game.Players.PlayerAdded:Connect(checkAndNotifyHR)
-        else
-            sendNotification("NOTIFICATION", "HR Notifier is DISABLED")
-        end
-    end
-})
 
  local Button = Tab:CreateButton({
 	Name = "Scan for HR's!",
